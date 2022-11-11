4e212f40c5f2dc05b503d3e63c263954071c9232
Kevin Sander <kevintsander@gmail.com>
2022-11-10 22:07:02 -0500

    added posts and comments

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,26 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_10_220255) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_11_025412) do
+  create_table "comments", force: :cascade do |t|
+    t.string "text"
+    t.integer "user_id", null: false
+    t.integer "post_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["post_id"], name: "index_comments_on_post_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.string "link"
+    t.integer "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "name", null: false
     t.string "email", null: false
@@ -18,4 +37,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_10_220255) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "comments", "posts"
+  add_foreign_key "comments", "users"
+  add_foreign_key "posts", "users"
 end
